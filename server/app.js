d04f741b9507dde59a53daa7d268a9367f2296f0
Renataclara <renataclarak@gmail.com>
2017-08-30 15:59:58 +0700

    done with additional installs

--- server/app.js
+++ server/app.js
@@ -4,11 +4,15 @@ var favicon = require('serve-favicon');
 var logger = require('morgan');
 var cookieParser = require('cookie-parser');
 var bodyParser = require('body-parser');
+var cors = require('cors');
 
 var index = require('./routes/index');
 var users = require('./routes/users');
 
 var app = express();
+
+var mongoose = require('mongoose');
+mongoose.connect('mongodb://renata:12345@atlas-start-shard-00-00-afgyk.mongodb.net:27017,atlas-start-shard-00-01-afgyk.mongodb.net:27017,atlas-start-shard-00-02-afgyk.mongodb.net:27017/beeshare?ssl=true&replicaSet=atlas-start-shard-0&authSource=admin');
 
 // view engine setup
 app.set('views', path.join(__dirname, 'views'));
@@ -16,6 +20,7 @@ app.set('view engine', 'jade');
 
 // uncomment after placing your favicon in /public
 //app.use(favicon(path.join(__dirname, 'public', 'favicon.ico')));
+app.use(cors());
 app.use(logger('dev'));
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: false }));
